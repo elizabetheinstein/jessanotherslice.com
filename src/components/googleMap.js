5cbd74389b52380b719c4a3eaae2adadedffac4e
Elizabeth Einstein <eeinstein134@gmail.com>
2020-06-18 21:31:02 -0600

    Added list of restaurants to map

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -1,16 +1,27 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import { MAPS_API_KEY } from "../../secrets.json"
+import { MAPS_API_KEY } from "../../secrets.json";
+
+import {
+    JoeAndPats,
+    Brunetti,
+    DiFara,
+    Macchina,
+    LBK,
+    BleekerStreetPizza,
+    NYPizzaSuprema,
+    Emily,
+    LIndustriePizzeria,
+    Keste
+} from "./restaurantList";
 
 const defaultProps = {
     center: {
         lat: 40.728953,
         lng: -73.984091,
     },
-    zoom: 12,
+    zoom: 13,
 };
-
-const Restaurant = ({ text }) => <div>{text}</div>;
 
 const GoogleMap = () => (
     <div style={{ height: '100vh', width: '100%' }}>
@@ -19,11 +30,16 @@ const GoogleMap = () => (
             defaultCenter={defaultProps.center}
             defaultZoom={defaultProps.zoom}
         >
-            <Restaurant
-                lat={40.728953}
-                lng={-73.984091}
-                text={"1"}
-            />
+            <JoeAndPats lat={40.728953} lng={-73.984091} text={"1"} />
+            <Brunetti lat={40.738268} lng={-74.0075657} text={"2"} />
+            <DiFara lat={40.7169342} lng={-73.9641728} text={"3"} />
+            <Macchina lat={40.740848} lng={-73.9833777} text={"4"} />
+            <LBK lat={40.6840676} lng={-73.9977192} text={"5"} />
+            <BleekerStreetPizza lat={40.7321938} lng={-74.0055907} text={"6"} />
+            <NYPizzaSuprema lat={40.7501438} lng={-73.9974131} text={"7"} />
+            <Emily lat={40.7501226} lng={-74.0652643} text={"8"} />
+            <LIndustriePizzeria lat={40.7116053} lng={-73.9600578} text={"9"} />
+            <Keste lat={40.7315345} lng={-74.0051513} text={"10"} />
         </GoogleMapReact>
     </div>
 );
